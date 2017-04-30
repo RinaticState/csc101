c2dbe7f066f4f464f7410b136c2336d21aaf5009
RinaticState <rinaticstates@gmail.com>
2017-04-29 22:33:13 -0400

    Added the second part of Project A2

---
+++ NguyenAA2/NguyenAA22/Form1.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gbTicketsSold = New System.Windows.Forms.GroupBox()
+        Me.txtClassC = New System.Windows.Forms.TextBox()
+        Me.txtClassB = New System.Windows.Forms.TextBox()
+        Me.txtClassA = New System.Windows.Forms.TextBox()
+        Me.lblClassC = New System.Windows.Forms.Label()
+        Me.lblClassB = New System.Windows.Forms.Label()
+        Me.lblClassA = New System.Windows.Forms.Label()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.gbRevenueGenerated = New System.Windows.Forms.GroupBox()
+        Me.lblTotalRevenue = New System.Windows.Forms.Label()
+        Me.lblClassCGenerated = New System.Windows.Forms.Label()
+        Me.lblClassBGenerated = New System.Windows.Forms.Label()
+        Me.lblClassAGenerated = New System.Windows.Forms.Label()
+        Me.lblTotalRevenueLabel = New System.Windows.Forms.Label()
+        Me.lblClassC1 = New System.Windows.Forms.Label()
+        Me.lblClassB1 = New System.Windows.Forms.Label()
+        Me.lblClassA1 = New System.Windows.Forms.Label()
+        Me.btnCalculateRevenue = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.gbTicketsSold.SuspendLayout()
+        Me.gbRevenueGenerated.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gbTicketsSold
+        '
+        Me.gbTicketsSold.Controls.Add(Me.txtClassC)
+        Me.gbTicketsSold.Controls.Add(Me.txtClassB)
+        Me.gbTicketsSold.Controls.Add(Me.txtClassA)
+        Me.gbTicketsSold.Controls.Add(Me.lblClassC)
+        Me.gbTicketsSold.Controls.Add(Me.lblClassB)
+        Me.gbTicketsSold.Controls.Add(Me.lblClassA)
+        Me.gbTicketsSold.Controls.Add(Me.lblInstructions)
+        Me.gbTicketsSold.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gbTicketsSold.Location = New System.Drawing.Point(14, 12)
+        Me.gbTicketsSold.Name = "gbTicketsSold"
+        Me.gbTicketsSold.Size = New System.Drawing.Size(252, 221)
+        Me.gbTicketsSold.TabIndex = 0
+        Me.gbTicketsSold.TabStop = False
+        Me.gbTicketsSold.Text = "Tickets Sold"
+        '
+        'txtClassC
+        '
+        Me.txtClassC.Location = New System.Drawing.Point(107, 167)
+        Me.txtClassC.Name = "txtClassC"
+        Me.txtClassC.Size = New System.Drawing.Size(65, 22)
+        Me.txtClassC.TabIndex = 6
+        '
+        'txtClassB
+        '
+        Me.txtClassB.Location = New System.Drawing.Point(107, 124)
+        Me.txtClassB.Name = "txtClassB"
+        Me.txtClassB.Size = New System.Drawing.Size(65, 22)
+        Me.txtClassB.TabIndex = 5
+        '
+        'txtClassA
+        '
+        Me.txtClassA.Location = New System.Drawing.Point(107, 81)
+        Me.txtClassA.Name = "txtClassA"
+        Me.txtClassA.Size = New System.Drawing.Size(65, 22)
+        Me.txtClassA.TabIndex = 4
+        '
+        'lblClassC
+        '
+        Me.lblClassC.AutoSize = True
+        Me.lblClassC.Location = New System.Drawing.Point(35, 170)
+        Me.lblClassC.Name = "lblClassC"
+        Me.lblClassC.Size = New System.Drawing.Size(57, 16)
+        Me.lblClassC.TabIndex = 3
+        Me.lblClassC.Text = "Class C:"
+        '
+        'lblClassB
+        '
+        Me.lblClassB.AutoSize = True
+        Me.lblClassB.Location = New System.Drawing.Point(35, 127)
+        Me.lblClassB.Name = "lblClassB"
+        Me.lblClassB.Size = New System.Drawing.Size(57, 16)
+        Me.lblClassB.TabIndex = 2
+        Me.lblClassB.Text = "Class B:"
+        '
+        'lblClassA
+        '
+        Me.lblClassA.AutoSize = True
+        Me.lblClassA.Location = New System.Drawing.Point(35, 84)
+        Me.lblClassA.Name = "lblClassA"
+        Me.lblClassA.Size = New System.Drawing.Size(57, 16)
+        Me.lblClassA.TabIndex = 1
+        Me.lblClassA.Text = "Class A:"
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInstructions.Location = New System.Drawing.Point(35, 31)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(178, 45)
+        Me.lblInstructions.TabIndex = 0
+        Me.lblInstructions.Text = "Enter the number of tickets sold for each class of seats"
+        '
+        'gbRevenueGenerated
+        '
+        Me.gbRevenueGenerated.Controls.Add(Me.lblTotalRevenue)
+        Me.gbRevenueGenerated.Controls.Add(Me.lblClassCGenerated)
+        Me.gbRevenueGenerated.Controls.Add(Me.lblClassBGenerated)
+        Me.gbRevenueGenerated.Controls.Add(Me.lblClassAGenerated)
+        Me.gbRevenueGenerated.Controls.Add(Me.lblTotalRevenueLabel)
+        Me.gbRevenueGenerated.Controls.Add(Me.lblClassC1)
+        Me.gbRevenueGenerated.Controls.Add(Me.lblClassB1)
+        Me.gbRevenueGenerated.Controls.Add(Me.lblClassA1)
+        Me.gbRevenueGenerated.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gbRevenueGenerated.Location = New System.Drawing.Point(282, 12)
+        Me.gbRevenueGenerated.Name = "gbRevenueGenerated"
+        Me.gbRevenueGenerated.Size = New System.Drawing.Size(252, 221)
+        Me.gbRevenueGenerated.TabIndex = 1
+        Me.gbRevenueGenerated.TabStop = False
+        Me.gbRevenueGenerated.Text = "Revenue Generated"
+        '
+        'lblTotalRevenue
+        '
+        Me.lblTotalRevenue.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalRevenue.Location = New System.Drawing.Point(142, 167)
+        Me.lblTotalRevenue.Name = "lblTotalRevenue"
+        Me.lblTotalRevenue.Size = New System.Drawing.Size(88, 20)
+        Me.lblTotalRevenue.TabIndex = 7
+        '
+        'lblClassCGenerated
+        '
+        Me.lblClassCGenerated.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblClassCGenerated.Location = New System.Drawing.Point(142, 130)
+        Me.lblClassCGenerated.Name = "lblClassCGenerated"
+        Me.lblClassCGenerated.Size = New System.Drawing.Size(88, 20)
+        Me.lblClassCGenerated.TabIndex = 8
+        '
+        'lblClassBGenerated
+        '
+        Me.lblClassBGenerated.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblClassBGenerated.Location = New System.Drawing.Point(142, 90)
+        Me.lblClassBGenerated.Name = "lblClassBGenerated"
+        Me.lblClassBGenerated.Size = New System.Drawing.Size(88, 20)
+        Me.lblClassBGenerated.TabIndex = 7
+        '
+        'lblClassAGenerated
+        '
+        Me.lblClassAGenerated.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblClassAGenerated.Location = New System.Drawing.Point(142, 49)
+        Me.lblClassAGenerated.Name = "lblClassAGenerated"
+        Me.lblClassAGenerated.Size = New System.Drawing.Size(88, 20)
+        Me.lblClassAGenerated.TabIndex = 6
+        '
+        'lblTotalRevenueLabel
+        '
+        Me.lblTotalRevenueLabel.AutoSize = True
+        Me.lblTotalRevenueLabel.Location = New System.Drawing.Point(31, 167)
+        Me.lblTotalRevenueLabel.Name = "lblTotalRevenueLabel"
+        Me.lblTotalRevenueLabel.Size = New System.Drawing.Size(100, 16)
+        Me.lblTotalRevenueLabel.TabIndex = 5
+        Me.lblTotalRevenueLabel.Text = "Total Revenue:"
+        '
+        'lblClassC1
+        '
+        Me.lblClassC1.AutoSize = True
+        Me.lblClassC1.Location = New System.Drawing.Point(74, 130)
+        Me.lblClassC1.Name = "lblClassC1"
+        Me.lblClassC1.Size = New System.Drawing.Size(57, 16)
+        Me.lblClassC1.TabIndex = 4
+        Me.lblClassC1.Text = "Class C:"
+        '
+        'lblClassB1
+        '
+        Me.lblClassB1.AutoSize = True
+        Me.lblClassB1.Location = New System.Drawing.Point(74, 91)
+        Me.lblClassB1.Name = "lblClassB1"
+        Me.lblClassB1.Size = New System.Drawing.Size(57, 16)
+        Me.lblClassB1.TabIndex = 3
+        Me.lblClassB1.Text = "Class B:"
+        '
+        'lblClassA1
+        '
+        Me.lblClassA1.AutoSize = True
+        Me.lblClassA1.Location = New System.Drawing.Point(74, 51)
+        Me.lblClassA1.Name = "lblClassA1"
+        Me.lblClassA1.Size = New System.Drawing.Size(57, 16)
+        Me.lblClassA1.TabIndex = 2
+        Me.lblClassA1.Text = "Class A:"
+        '
+        'btnCalculateRevenue
+        '
+        Me.btnCalculateRevenue.Location = New System.Drawing.Point(88, 251)
+        Me.btnCalculateRevenue.Name = "btnCalculateRevenue"
+        Me.btnCalculateRevenue.Size = New System.Drawing.Size(127, 28)
+        Me.btnCalculateRevenue.TabIndex = 2
+        Me.btnCalculateRevenue.Text = "Calculate Revenue"
+        Me.btnCalculateRevenue.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(236, 251)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(92, 28)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(346, 251)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(92, 28)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(548, 322)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculateRevenue)
+        Me.Controls.Add(Me.gbRevenueGenerated)
+        Me.Controls.Add(Me.gbTicketsSold)
+        Me.Name = "Form1"
+        Me.Text = "Stadium Seating"
+        Me.gbTicketsSold.ResumeLayout(False)
+        Me.gbTicketsSold.PerformLayout()
+        Me.gbRevenueGenerated.ResumeLayout(False)
+        Me.gbRevenueGenerated.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents gbTicketsSold As GroupBox
+    Friend WithEvents txtClassC As TextBox
+    Friend WithEvents txtClassB As TextBox
+    Friend WithEvents txtClassA As TextBox
+    Friend WithEvents lblClassC As Label
+    Friend WithEvents lblClassB As Label
+    Friend WithEvents lblClassA As Label
+    Friend WithEvents lblInstructions As Label
+    Friend WithEvents gbRevenueGenerated As GroupBox
+    Friend WithEvents lblTotalRevenue As Label
+    Friend WithEvents lblClassCGenerated As Label
+    Friend WithEvents lblClassBGenerated As Label
+    Friend WithEvents lblClassAGenerated As Label
+    Friend WithEvents lblTotalRevenueLabel As Label
+    Friend WithEvents lblClassC1 As Label
+    Friend WithEvents lblClassB1 As Label
+    Friend WithEvents lblClassA1 As Label
+    Friend WithEvents btnCalculateRevenue As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ NguyenAA2/NguyenAA22/Form1.vb
@@ -0,0 +1,90 @@
+﻿'	Program:  Stadium Seating Calculator
+'	Written by: Amber N Nguyen
+'	Date:  30 September 2016
+'	Description: This program calculates ticket prices based on ticket price and number of purchases. There are three seperate tickets depending on the seating with their own prices.
+'                After calculating all the prices, all the prices are totaled up to determine the total amount of revenue.
+'	Challenges: First, I didn't know how to get the money values to show up (dblX.ToString("c") ).
+'               Second, I was confused as to how to put the .ToString("c") for the total revenue, but I realised that I had to declare another double and set the operations for that, so I could then convert the double to a string.
+'               Third, for the dblTOTAL_REVENUE calcuations, I realised that I didn't need to use any parenthesises.
+'	Time Spent:	2.5 hours
+'		User Input => Expected Output
+'		---------------------------------------------------------------  
+'		Class A: 50 => $750.00 ; Class B: 23 => $276.00 ; Class C: 62 => $558.00 ; => Total Revenue: $1,584.00
+'		Class A: 93 => $1395.00 ; Class B: 169 => $2028.00 ; Class C: 500 => $4500.00 ; => Total Revenue: $7,923.00
+'		Class A: 2000 => $30,000.00 ; Class B: 5000 => $60,000.00 ; Class C: 10000 => $90,000.00 ; => Total Revenue: $180,000.00
+
+Public Class Form1
+    'Declares the constant dblCLASSA_A as a Double.
+    Const dblCLASS_A As Double = 15
+
+    'Declares the constant dblCLASS_B as a Double.
+    Const dblCLASS_B As Double = 12
+
+    'Declares the constant dblCLASS_C as a Double.
+    Const dblCLASS_C As Double = 9
+    Private Sub btnCalculateRevenue_Click(sender As Object, e As EventArgs) Handles btnCalculateRevenue.Click
+        'Declares variable dblCLASS_A_TOTAL as a Double.
+        Dim dblCLASS_A_TOTAL As Double
+
+        'I am setting the operation for the variable dblCLASS_A_TOTAL to multiply the user input from txtClassA with the constant dblCLASS_A
+        dblCLASS_A_TOTAL = CDbl(txtClassA.Text) * (dblCLASS_A)
+
+        'Sets the label box to display the calculations from dblCLASS_A_TOTAL 's operation.
+        lblClassAGenerated.Text = dblCLASS_A_TOTAL.ToString("c")
+
+        'Declares variable dblClass_B_Total as a Double.
+        Dim dblCLASS_B_TOTAL As Double
+
+        'Sets up the operation for dblCLASS_B_TOTAL to multiply the user input from txtClassB with the constant dblCLASS_B
+        dblCLASS_B_TOTAL = CDbl(txtClassB.Text) * (dblCLASS_B)
+
+        'Sets the label box to display the calculations from dblCLASS_B_TOTAL 's operation.
+        lblClassBGenerated.Text = dblCLASS_B_TOTAL.ToString("c")
+
+        'Declares variable dblClass_C_Total as a Double.
+        Dim dblCLASS_C_TOTAL As Double
+
+        'Sets up the operation for dblCLASS_C_TOTAL to multiply the user input from txtClassC with the constant dblCLASS_C
+        dblCLASS_C_TOTAL = CDbl(txtClassC.Text) * (dblCLASS_C)
+
+        'Sets the label box to display the calculations from dblCLASS_C_TOTAL 's operation.
+        lblClassCGenerated.Text = dblCLASS_C_TOTAL.ToString("c")
+
+        'Declares variable dblTOTAL_REVENUE as a Double.
+        Dim dblTOTAL_REVENUE As Double
+
+        'Sets up the operation of dblTOTAL_REVENUE to add the totals from dblCLASS_A_TOTAL , dblCLASS_B_TOTAL , and dblCLASS_C_TOTAL
+        dblTOTAL_REVENUE = dblCLASS_A_TOTAL + dblCLASS_B_TOTAL + dblCLASS_C_TOTAL
+
+        'Sets the label box to display the calculations from dblTOTAL_REVENUE 's operation.
+        lblTotalRevenue.Text = dblTOTAL_REVENUE.ToString("c")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clears the Class A user input textbox.
+        txtClassA.Clear()
+
+        'Clears the Class B user input textbox.
+        txtClassB.Clear()
+
+        'Clears the Class C user input textbox.
+        txtClassC.Clear()
+
+        'Clears the label box.
+        lblClassAGenerated.Text = String.Empty
+
+        'Clears the label box.
+        lblClassBGenerated.Text = String.Empty
+
+        'Clears the label box.
+        lblClassCGenerated.Text = String.Empty
+
+        'Clears the label box.
+        lblTotalRevenue.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Closes the application.
+        Me.Close()
+    End Sub
+End Class
