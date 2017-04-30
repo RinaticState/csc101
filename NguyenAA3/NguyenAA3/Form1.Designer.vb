6870fcdcfbc075ba4b7524c93bf33ebc0f35d975
RinaticState <rinaticstates@gmail.com>
2017-04-29 22:39:17 -0400

    Added Project A3

---
+++ NguyenAA3/NguyenAA3/Form1.Designer.vb
@@ -0,0 +1,301 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.gbFoods = New System.Windows.Forms.GroupBox()
+        Me.btnCalculateTotal = New System.Windows.Forms.Button()
+        Me.txtFrenchFries = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtHamburgers = New System.Windows.Forms.TextBox()
+        Me.lblDontUseHamPrice = New System.Windows.Forms.Label()
+        Me.txtHotDogs = New System.Windows.Forms.TextBox()
+        Me.lblDontUseHotDogsPrice = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblDontUseTotal = New System.Windows.Forms.Label()
+        Me.lblTotalPrice = New System.Windows.Forms.Label()
+        Me.gbCalculations = New System.Windows.Forms.GroupBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblChangeDue = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnCalculateChange = New System.Windows.Forms.Button()
+        Me.txtCustomerPayment = New System.Windows.Forms.TextBox()
+        Me.lblDoNotUseOnceAgain = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblDontEvenUseYouFool = New System.Windows.Forms.Label()
+        Me.gbFoods.SuspendLayout()
+        Me.gbCalculations.SuspendLayout()
+        Me.lblStatus.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gbFoods
+        '
+        Me.gbFoods.Controls.Add(Me.btnCalculateTotal)
+        Me.gbFoods.Controls.Add(Me.txtFrenchFries)
+        Me.gbFoods.Controls.Add(Me.Label2)
+        Me.gbFoods.Controls.Add(Me.txtHamburgers)
+        Me.gbFoods.Controls.Add(Me.lblDontUseHamPrice)
+        Me.gbFoods.Controls.Add(Me.txtHotDogs)
+        Me.gbFoods.Controls.Add(Me.lblDontUseHotDogsPrice)
+        Me.gbFoods.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gbFoods.Location = New System.Drawing.Point(21, 57)
+        Me.gbFoods.Name = "gbFoods"
+        Me.gbFoods.Size = New System.Drawing.Size(198, 284)
+        Me.gbFoods.TabIndex = 0
+        Me.gbFoods.TabStop = False
+        Me.gbFoods.Text = "Food"
+        '
+        'btnCalculateTotal
+        '
+        Me.btnCalculateTotal.Location = New System.Drawing.Point(37, 228)
+        Me.btnCalculateTotal.Name = "btnCalculateTotal"
+        Me.btnCalculateTotal.Size = New System.Drawing.Size(122, 30)
+        Me.btnCalculateTotal.TabIndex = 4
+        Me.btnCalculateTotal.Text = "Calculate Total"
+        Me.btnCalculateTotal.UseVisualStyleBackColor = True
+        '
+        'txtFrenchFries
+        '
+        Me.txtFrenchFries.Location = New System.Drawing.Point(44, 189)
+        Me.txtFrenchFries.Name = "txtFrenchFries"
+        Me.txtFrenchFries.Size = New System.Drawing.Size(100, 24)
+        Me.txtFrenchFries.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(29, 154)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(135, 22)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "French Fries: $0.75"
+        '
+        'txtHamburgers
+        '
+        Me.txtHamburgers.Location = New System.Drawing.Point(44, 119)
+        Me.txtHamburgers.Name = "txtHamburgers"
+        Me.txtHamburgers.Size = New System.Drawing.Size(100, 24)
+        Me.txtHamburgers.TabIndex = 2
+        '
+        'lblDontUseHamPrice
+        '
+        Me.lblDontUseHamPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDontUseHamPrice.Location = New System.Drawing.Point(29, 88)
+        Me.lblDontUseHamPrice.Name = "lblDontUseHamPrice"
+        Me.lblDontUseHamPrice.Size = New System.Drawing.Size(135, 22)
+        Me.lblDontUseHamPrice.TabIndex = 2
+        Me.lblDontUseHamPrice.Text = "Hamburgers: $1.50"
+        '
+        'txtHotDogs
+        '
+        Me.txtHotDogs.Location = New System.Drawing.Point(44, 56)
+        Me.txtHotDogs.Name = "txtHotDogs"
+        Me.txtHotDogs.Size = New System.Drawing.Size(100, 24)
+        Me.txtHotDogs.TabIndex = 1
+        '
+        'lblDontUseHotDogsPrice
+        '
+        Me.lblDontUseHotDogsPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDontUseHotDogsPrice.Location = New System.Drawing.Point(36, 28)
+        Me.lblDontUseHotDogsPrice.Name = "lblDontUseHotDogsPrice"
+        Me.lblDontUseHotDogsPrice.Size = New System.Drawing.Size(118, 22)
+        Me.lblDontUseHotDogsPrice.TabIndex = 0
+        Me.lblDontUseHotDogsPrice.Text = "Hot Dogs: $1.00"
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(45, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(150, 35)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter the quantity of the food the customer will purchase."
+        '
+        'lblDontUseTotal
+        '
+        Me.lblDontUseTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDontUseTotal.Location = New System.Drawing.Point(57, 24)
+        Me.lblDontUseTotal.Name = "lblDontUseTotal"
+        Me.lblDontUseTotal.Size = New System.Drawing.Size(91, 22)
+        Me.lblDontUseTotal.TabIndex = 4
+        Me.lblDontUseTotal.Text = "Total Price:"
+        '
+        'lblTotalPrice
+        '
+        Me.lblTotalPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalPrice.Location = New System.Drawing.Point(48, 48)
+        Me.lblTotalPrice.Name = "lblTotalPrice"
+        Me.lblTotalPrice.Size = New System.Drawing.Size(100, 24)
+        Me.lblTotalPrice.TabIndex = 5
+        '
+        'gbCalculations
+        '
+        Me.gbCalculations.Controls.Add(Me.btnClear)
+        Me.gbCalculations.Controls.Add(Me.lblChangeDue)
+        Me.gbCalculations.Controls.Add(Me.Label3)
+        Me.gbCalculations.Controls.Add(Me.btnCalculateChange)
+        Me.gbCalculations.Controls.Add(Me.txtCustomerPayment)
+        Me.gbCalculations.Controls.Add(Me.lblDoNotUseOnceAgain)
+        Me.gbCalculations.Controls.Add(Me.lblTotalPrice)
+        Me.gbCalculations.Controls.Add(Me.lblDontUseTotal)
+        Me.gbCalculations.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gbCalculations.Location = New System.Drawing.Point(264, 57)
+        Me.gbCalculations.Name = "gbCalculations"
+        Me.gbCalculations.Size = New System.Drawing.Size(199, 284)
+        Me.gbCalculations.TabIndex = 2
+        Me.gbCalculations.TabStop = False
+        Me.gbCalculations.Text = "Calculations"
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(57, 237)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(91, 29)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lblChangeDue
+        '
+        Me.lblChangeDue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblChangeDue.Location = New System.Drawing.Point(51, 205)
+        Me.lblChangeDue.Name = "lblChangeDue"
+        Me.lblChangeDue.Size = New System.Drawing.Size(100, 24)
+        Me.lblChangeDue.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(54, 176)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(94, 18)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Change Due:"
+        '
+        'btnCalculateChange
+        '
+        Me.btnCalculateChange.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculateChange.Location = New System.Drawing.Point(32, 141)
+        Me.btnCalculateChange.Name = "btnCalculateChange"
+        Me.btnCalculateChange.Size = New System.Drawing.Size(140, 27)
+        Me.btnCalculateChange.TabIndex = 8
+        Me.btnCalculateChange.Text = "Calculate Change"
+        Me.btnCalculateChange.UseVisualStyleBackColor = True
+        '
+        'txtCustomerPayment
+        '
+        Me.txtCustomerPayment.Location = New System.Drawing.Point(48, 105)
+        Me.txtCustomerPayment.Name = "txtCustomerPayment"
+        Me.txtCustomerPayment.Size = New System.Drawing.Size(100, 26)
+        Me.txtCustomerPayment.TabIndex = 7
+        '
+        'lblDoNotUseOnceAgain
+        '
+        Me.lblDoNotUseOnceAgain.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDoNotUseOnceAgain.Location = New System.Drawing.Point(24, 79)
+        Me.lblDoNotUseOnceAgain.Name = "lblDoNotUseOnceAgain"
+        Me.lblDoNotUseOnceAgain.Size = New System.Drawing.Size(142, 22)
+        Me.lblDoNotUseOnceAgain.TabIndex = 6
+        Me.lblDoNotUseOnceAgain.Text = "Customer Payment:"
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(204, 345)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 30)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.lblStatus.Location = New System.Drawing.Point(0, 379)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(487, 22)
+        Me.lblStatus.TabIndex = 4
+        Me.lblStatus.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
+        '
+        'lblDontEvenUseYouFool
+        '
+        Me.lblDontEvenUseYouFool.Location = New System.Drawing.Point(293, 15)
+        Me.lblDontEvenUseYouFool.Name = "lblDontEvenUseYouFool"
+        Me.lblDontEvenUseYouFool.Size = New System.Drawing.Size(139, 32)
+        Me.lblDontEvenUseYouFool.TabIndex = 5
+        Me.lblDontEvenUseYouFool.Text = "Enter any calculations you wish to make."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(487, 401)
+        Me.Controls.Add(Me.lblDontEvenUseYouFool)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.gbCalculations)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.gbFoods)
+        Me.Name = "Form1"
+        Me.Text = "Hot Dog Stand Cashier Program"
+        Me.gbFoods.ResumeLayout(False)
+        Me.gbFoods.PerformLayout()
+        Me.gbCalculations.ResumeLayout(False)
+        Me.gbCalculations.PerformLayout()
+        Me.lblStatus.ResumeLayout(False)
+        Me.lblStatus.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents gbFoods As GroupBox
+    Friend WithEvents btnCalculateTotal As Button
+    Friend WithEvents txtFrenchFries As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtHamburgers As TextBox
+    Friend WithEvents lblDontUseHamPrice As Label
+    Friend WithEvents txtHotDogs As TextBox
+    Friend WithEvents lblDontUseHotDogsPrice As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblDontUseTotal As Label
+    Friend WithEvents lblTotalPrice As Label
+    Friend WithEvents gbCalculations As GroupBox
+    Friend WithEvents lblChangeDue As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnCalculateChange As Button
+    Friend WithEvents txtCustomerPayment As TextBox
+    Friend WithEvents lblDoNotUseOnceAgain As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblStatus As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents lblDontEvenUseYouFool As Label
+End Class
---
+++ NguyenAA3/NguyenAA3/Form1.vb
@@ -0,0 +1,161 @@
+﻿'	Program:  Hot Dog Stand Cashier Program
+'	Written by:  	Amber N Nguyen
+'	Date:  11/04/16
+'	Description: This program is intended as use for calculating prices as a cashier for a hot dog stand. It displays the prices
+'       of the food alongside its name. It also calculates the price of the food with the 6.25% Mass Sales Tax. When the customer
+'       hands the cashier the change, the user will be able to enter the amount and get the change.
+'	Challenges: First, I was confused with TryParse statements and how to write them. I had to look at the textbook on how to
+'       write them. Second, I did not know how to get the value of the total price from the variable "dblTotalPrice" since it was
+'       in one procedure, and I had to use it for another procedure. Initially, I asked my friend, and she said I could use the
+'       price output from the label, lblTotalPrice. I used the label's text output to subtract it with the dblChange variable.
+'       However, when I showed Professor Hager the code, she said I could have just declared a class level variable. I must have
+'       forgotten how to do that, so I looked up a Youtube video. Third, and still my only problem I do not know how to solve is
+'       that when I click "calculate change," the change will appear with parentheses around the numbers. I'm not really sure
+'       where the problem for that lies.. Update: I had to redo the TryParse Statements once again, but I understand them better >_>
+'       Also, I had to rename my variables. Now I know how to properly name constants and doubles. I can't trust my old codes..
+'       Another update: I found out the source of the parentheses problem. I was doing dblTotalPrice - dblChange, which is wrong
+'       because the change should be more than the total price. VB also doesn't display the - sign for currency.
+'	Time Spent:	4 hours 
+'		User Input:	Expected Output:
+'		---------------------------------------------------------------  
+'		Hot Dogs: 5 ; Hamburgers: 1 ; French Fries: 0 => Total: $6.91 => Customer Payment: $10 => Change: $3.09
+'		Hot Dogs: 60 ; Hamburgers: 50 ; French Fries: 100 => Total: $223.13 => Customer Payment: $300.13 => Change: $77.00
+'		Hot Dogs: 0 ; Hamburgers: 2 ; French Fries: 0 => Total: $3.19 => Customer Payment: $3.19 => Change: $0.00
+'
+
+Public Class Form1
+    'Declares constants for the prices of the foods.
+
+    Const dblHOT_DOGS As Double = 1         'Hot Dog Price Is $1.
+
+    Const dblHAMBURGERS As Double = 1.5     'Hamburger Price Is $1.50.
+
+    Const dblFRENCH_FRIES As Double = 0.75  'French Fries Price Is $0.75.
+
+    Const dblMASS_TAX As Double = 0.0625    'Mass Sales Tax Is 6.25%.
+
+    'Declares a class level variable for the total price of the foods.
+
+    Dim dblTotalPrice As Double             'dblTotalPrice will be used in the CalculateTotal Event Handler and Calculate Change Event Handler.
+
+    Private Sub btnCalculateTotal_Click(sender As Object, e As EventArgs) Handles btnCalculateTotal.Click
+        'Declaring variables as double.
+
+        Dim dblHotDogPrice As Double            'This variable is for the hot dog price: its price multiplied by quantity.
+
+        Dim dblHamburgerPrice As Double         'This variable is for the hamburger price: its price multiplied by quantity.
+
+        Dim dblFrenchFriesPrice As Double       'This variable is for the french fries price: its price multiplied by quantity.
+
+        Dim dblMassSalesTax As Double           'This variable is to have the total price multiplied by the 6.25% Mass Sales Tax.
+
+        Dim dblNonTaxPrice As Double            'This variable is for the total price without the Mass Sales Tax.
+
+        'The operation is in a try-catch block for exception handling.
+
+        Try
+
+            'This TryParse statement serves to validate user input and an operation for the hot dog pricing calculations.
+            'It uses the variable "dblHotDogPrice" to multiply the hot dog's price with the quantity of hot dog orders.
+            If Double.TryParse(txtHotDogs.Text, dblHOT_DOGS) Then
+                dblHotDogPrice = CDbl(txtHotDogs.Text) * dblHOT_DOGS
+            Else
+                ToolStripStatusLabel1.Text = "Please enter a numeric value for the hot dogs."
+            End If
+
+            'This TryParse statement serves validate user input and an operation for the hamburger pricing calculations.
+            'It uses the variable "dblHamburger" to multiply the hamburger's price with the quantity of hamburger orders.
+            If Double.TryParse(txtHamburgers.Text, dblHAMBURGERS) Then
+                dblHamburgerPrice = CDbl(txtHamburgers.Text) * dblHAMBURGERS
+            Else
+                ToolStripStatusLabel1.Text = "Please enter a numeric value for the hamburgers."
+            End If
+
+            'This TryParse statement serves validate user input and an operation for the french fries pricing calculations.
+            'It uses the variable "dblFrenchFriesPrice" to multiply the french fries' price with the quantity of french fries orders.
+            If Double.TryParse(txtFrenchFries.Text, dblFRENCH_FRIES) Then
+                dblFrenchFriesPrice = CDbl(txtFrenchFries.Text) * dblFRENCH_FRIES
+            Else
+                ToolStripStatusLabel1.Text = "Please enter a numeric value for the french fries."
+            End If
+
+            'This catch part is to send a message to the user instead of crashing (although the try-parse statements tell you what you're doing wrong).
+        Catch ex As Exception
+            ToolStripStatusLabel1.Text = "Please enter numeric values for the food."
+        End Try
+
+        'Calculates the total prices of each foods in accordance with their quantity together. It does not include the sales tax.
+        dblNonTaxPrice = dblHotDogPrice + dblHamburgerPrice + dblFrenchFriesPrice
+
+        'Multiples the 6.25% Mass Sales Tax with the total price.
+        dblMassSalesTax = dblNonTaxPrice * dblMASS_TAX
+
+        'This adds the sales tax to the price of the non sales tax.
+        dblTotalPrice = dblNonTaxPrice + dblMassSalesTax
+
+        'Displays the output for the total price.
+        lblTotalPrice.Text = dblTotalPrice.ToString("c")
+
+        'The message box pops up a message that summarises the order.
+        MessageBox.Show("The order includes " & txtHotDogs.Text & " hot dogs, " & txtHamburgers.Text & " hamburgers, and " & txtFrenchFries.Text & " French Fries, for the total of " & dblTotalPrice.ToString("c"))
+    End Sub
+
+    Private Sub btnCalculateChange_Click(sender As Object, e As EventArgs) Handles btnCalculateChange.Click
+        'Declaring variables as double.
+
+        Dim dblChange As Double             'Declares the variable for money the customer gives to the cashier.
+
+        Dim dblChangeTotal As Double        'Declares the variable for the change to give back to the customer.
+
+        'The operation is enclosed in a try-catch block.
+        Try
+
+            If Double.TryParse(txtCustomerPayment.Text, dblChange) Then
+                dblChange = CDbl(txtCustomerPayment.Text)
+            Else
+                ToolStripStatusLabel1.Text = "Please enter a numeric value for the customer change."
+            End If
+
+            'Catches an error in case the user makes a mistake.
+        Catch ex As Exception
+            ToolStripStatusLabel1.Text = "Please enter valid numeric input for the customer change."
+        End Try
+
+        'Subtracts the input of customer payment from the total price.
+        dblChangeTotal = dblChange - dblTotalPrice
+
+        'Displays the amount of change due to the customer.
+        lblChangeDue.Text = dblChangeTotal.ToString("c")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clears the form.
+
+        'Clears the hot dogs textbox input.
+        txtHotDogs.Clear()
+
+        'Clears the hamburgers textbox.
+        txtHamburgers.Clear()
+
+        'Clears the french fries textbox.
+        txtFrenchFries.Clear()
+
+        'Clears the label displaying total price.
+        lblTotalPrice.Text = String.Empty
+
+        'Clears the customer payment textbox.
+        txtCustomerPayment.Clear()
+
+        'Clears the change due label.
+        lblChangeDue.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Closes the application.
+        Me.Close()
+    End Sub
+
+    Private Sub txtHotDogs_TextChanged(sender As Object, e As EventArgs) Handles txtHotDogs.TextChanged
+
+    End Sub
+End Class
